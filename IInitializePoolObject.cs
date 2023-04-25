90cb9cc6e080c3908ea69f2b5982061867e6d338
Igor Vasilev <Protanker25@gmail.com>
2023-04-25 23:12:18 +1000

    Initial commit

---
+++ IInitializePoolObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EnotoButerbrodo
+{
+    public interface IInitializePoolObject<T> : IPoolObject
+    {
+        public void Initialize(Action<T> returnAction);
+
+        public void ReturnToPool();
+    }
+
+    public interface IPoolObject
+    {
+        void ReturnToPool();
+    }
+}
---
+++ ObjectsPool.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnotoButerbrodo
+{
+    public class ObjectsPool<T> where T : MonoBehaviour, IInitializePoolObject<T>
+    {
+        public event Action<T> ObjectReturned;
+        public int Count => _pooledObjects.Count;
+        public Type ObjectType => typeof(T);
+        
+        private readonly Stack<T> _pooledObjects;
+        private readonly T _pooledObjectReference;
+        private Transform _parent;  
+        
+        public ObjectsPool(T objectReference, int startCapacity = 8, Transform parent = null)
+        {
+            _pooledObjects = new Stack<T>(startCapacity);
+            _pooledObjectReference = objectReference;
+            _parent = parent;
+            
+            for (int i = 0; i < startCapacity; i++) 
+            {
+                PoolNewObject();
+            }
+        }
+
+        public T Get()
+        {
+            if (Count == 0)
+            {
+                PoolNewObject();    
+            }
+            
+            var poolObject = _pooledObjects.Pop();
+            poolObject.gameObject.SetActive(true);
+            
+            return poolObject;
+        }
+        private void PoolNewObject()
+        {
+            T newObject = GameObject.Instantiate(_pooledObjectReference
+                ,Vector3.zero
+                ,Quaternion.identity
+                ,_parent);
+            
+            newObject.gameObject.SetActive(false);
+            newObject.Initialize(ReturnAction);
+            _pooledObjects.Push(newObject);
+        }
+
+        private void ReturnAction(T poolObject)
+        {
+            poolObject.gameObject.SetActive(false);
+            _pooledObjects.Push(poolObject);
+        }
+    }
+}
